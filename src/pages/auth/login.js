ab4d18fed994fb30784bab8ed781c7a26b991f08
Woehammer <72220895+Woehammer@users.noreply.github.com>
2025-06-26 12:08:52 +0100

    Update login.js

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { supabase } from '../../lib/supabaseclient';
+import { supabase } from '../../lib/supabaseClient';
 
 export default function Login() {
   const router = useRouter();
