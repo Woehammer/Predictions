73b04ef31f90e452169c0d3537a4fc20b0994e1e
Woehammer <72220895+Woehammer@users.noreply.github.com>
2025-06-26 12:46:26 +0100

    Create create-league.js

---
+++ src/pages/create-league.js
@@ -0,0 +1,8 @@
+export default function CreateLeague() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Create a New League</h1>
+      {/* Add your league creation form here */}
+    </div>
+  );
+}
