6861e147ecfe8254ecc42fdac37ec8642150dc1d
Woehammer <72220895+Woehammer@users.noreply.github.com>
2025-06-26 14:47:38 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import { useState } from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-react';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
+
+export default MyApp;
