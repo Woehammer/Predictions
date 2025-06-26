9b52ee8b8f798ecb40490bbde4c52237e39d7115
Woehammer <72220895+Woehammer@users.noreply.github.com>
2025-06-26 12:14:19 +0100

    Update predictions.js

--- src/pages/predictions.js
+++ src/pages/predictions.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../lib/supabaseclient';
 
 export default function Predictions() {
   const [fixtures, setFixtures] = useState([]);
