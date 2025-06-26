e7df9c9fcc524fcfd810cda3757e91458eef66a8
Woehammer <72220895+Woehammer@users.noreply.github.com>
2025-06-26 12:15:06 +0100

    Update signup.js

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseclient';
 // import Header from '../../components/Header'; // optional
 
 export default function Signup() {
