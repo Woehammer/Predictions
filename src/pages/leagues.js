b3dcaefd80f975a650863b736b520dd27bbed485
Woehammer <72220895+Woehammer@users.noreply.github.com>
2025-06-26 17:27:42 +0100

    Update leagues.js

--- src/pages/leagues.js
+++ src/pages/leagues.js
@@ -1,11 +1,1 @@
-// pages/leagues.js
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
-export default function RedirectToDashboard() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push('/dashboard');
-  }, []);
-  return null;
-}
