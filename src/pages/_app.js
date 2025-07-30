8a30a8e61fd7d15dc57e714c8efa14c110f59cea
Woehammer <72220895+Woehammer@users.noreply.github.com>
2025-07-30 14:30:29 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import '@/styles/globals.css';
 import { useState } from 'react';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
